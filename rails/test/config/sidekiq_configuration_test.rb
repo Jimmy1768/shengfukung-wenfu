22ea6f1f19ffba9479b7176788f33b90e5555e8a
Chu-Lin Chuang <jimmy1768@Chu-Lins-MacBook-Pro.local>
2026-07-13 12:21:28 +0800

    fix: restore sidekiq boot

---
+++ rails/test/config/sidekiq_configuration_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "erb"
+require "yaml"
+
+class SidekiqConfigurationTest < ActiveSupport::TestCase
+  test "configuration renders before Rails boots" do
+    path = Rails.root.join("config/sidekiq.yml")
+    rendered = Dir.chdir(Rails.root) { ERB.new(path.read).result }
+    config = YAML.safe_load(rendered, permitted_classes: [Symbol], aliases: true)
+
+    assert_equal "./config/environment", config.fetch(:require)
+    assert_equal Profile::Infrastructure::JOB_CONCURRENCY, config.fetch(:concurrency)
+    assert_equal Profile::Infrastructure::JobQueues.ordered.map(&:to_s), config.fetch(:queues)
+  end
+end
